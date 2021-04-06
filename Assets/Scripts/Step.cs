ddc5d7fb4eb12ae439b043c8307819879fdb207c
Aleksandr Zakrevskij <ctapocta_210@mail.ru>
2021-04-06 13:37:23 +0300

    Текстовый квест (Урок)

---
+++ Assets/Scripts/Step.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Step : MonoBehaviour
+{
+    #region Variables
+    [TextArea(1,10)]
+    public string contentText;
+
+    public Step[] nextSteps;
+
+    #endregion
+}
---
+++ Assets/Scripts/TextQuest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TextQuest : MonoBehaviour
+{
+    #region Variables
+    public Text contentLabel;
+    public Step startStep;
+    private Step currentStep;
+    #endregion
+    #region Unity livecycle
+
+    private void Start()
+    {
+        currentStep = startStep;
+        UpdateContentLabel();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            //Step nextStep = currentStep.nextStep1;
+            MoveToNextStep(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            MoveToNextStep(1);
+        }
+    }
+
+    #endregion
+    #region Public methods
+
+    
+
+    #endregion
+    #region Private methods
+
+    private void UpdateContentLabel()
+    {
+        contentLabel.text = currentStep.contentText;
+    }
+
+    private void MoveToNextStep(int stepIndex)
+    {
+        if (currentStep.nextSteps.Length > stepIndex)
+        {
+            currentStep = currentStep.nextSteps[stepIndex];
+            UpdateContentLabel();
+        }
+    }
+    
+    #endregion
+    
+    
+}
